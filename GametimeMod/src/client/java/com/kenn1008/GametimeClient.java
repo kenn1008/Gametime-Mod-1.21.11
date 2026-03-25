22ff4fd65cc88648e0b70ea9c8be4a4a2ff94a39
kenn1008 <73196267+kenn1008@users.noreply.github.com>
2026-03-25 03:01:50 -0500

    Add files via upload

---
+++ GametimeMod/src/client/java/com/kenn1008/GametimeClient.java
@@ -0,0 +1,10 @@
+package com.kenn1008;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GametimeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
